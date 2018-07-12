29455a1eb45e5b112143f860ca593b87e6a1e2d3
ImmaBird <immapokeyew@gmail.com>
2018-07-11 20:08:06 -0400

    some gui

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,24 +1,68 @@
 import javafx.application.Application;
+import javafx.event.EventType;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-class ChatClient extends Application {
+public class ChatClient extends Application {
+
+	private String name;
+	private String ip;
+	private int port;
 
 	public static void main(String[] args) {
 		Application.launch();
 	}
 
-	private static int width = 600;
-	private static int height = 400;
-
 	public void start(Stage stage) {
-		Pane pane = new Pane();
-		stage.setScene(new Scene(pane, width, height));
+		stage.setTitle("Secure Chat");
+		showMenu(stage);
 		stage.show();
-		while (true) {
+	}
 
-		}
+	private void showMenu(Stage stage){
+		int width = 300;
+		int height = 200;
+
+		StackPane menuPane = new StackPane();
+
+		VBox vb = new VBox();
+		menuPane.getChildren().add(vb);
+
+		Label nameLabel = new Label("Name");
+		TextField nameField = new TextField();
+
+		Label ipLabel = new Label("IP");
+		TextField ipField = new TextField();
+
+		Button submit = new Button("Submit");
+
+		vb.getChildren().addAll(nameLabel, nameField, ipLabel, ipField, submit);
+
+		stage.setScene(new Scene(menuPane, width, height));
+	}
+
+	private void showChat(Stage stage){
+		int width = 600;
+		int height = 400;
+
+		BorderPane chatPane = new BorderPane();
+
+		ListView<String> chatArea = new ListView<String>();
+
+		chatPane.getChildren().add(chatArea);
+
+		stage.setScene(new Scene(chatPane, width, height));
 	}
 
 }
